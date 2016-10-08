450eefa9abe63d59078c63ce01e6138d193cbfb7
BenChan2016 <ckchanapben@gmail.com>
2016-10-08 10:07:25 +0800

    functions and implementation are seperated. Regular expression added

---
+++ functions.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  8 09:44:28 2016
+
+@author: ben
+"""
+
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+import re   #regular expression
+from pandas.tools.plotting import scatter_matrix
+import matplotlib.pyplot as plt
+import numpy as np
+import time # to measure performance
+
+
+
+"User task: Ask user to inputs a file location and return xlxs"
+def user_input_file_location():
+    usrinput = input(">>> Input: ")
+    print("Your file location is "+usrinput)
+    return usrinput;
+    
+"System task: read xlxs and turn it into dataframe"
+def system_input_file(usrinput):
+    file_extension = detect_file_extension(usrinput)
+    
+    if(file_extension.lower()=="xlsx"):
+        file_input_to_system = pd.read_excel(usrinput)
+    if(file_extension.lower()=="csv"):
+        file_input_to_system = pd.read_csv(usrinput)
+    
+    
+    print("\n")
+    print(file_input_to_system.head())
+    return file_input_to_system
+    
+def detect_file_extension(usrinput):
+    file_extension = re.split("\.",usrinput) #get the extension name
+    return file_extension[1]
+
+
+def drop_space_nondigital(df):
+    data_frame = df.replace('\s+', np.nan, regex=True)   #http://stackoverflow.com/questions/26837998/pandas-replace-nan-with-blank-empty-string
+    data_frame = df.replace('[^\d]', np.nan, regex=True) #change the field to null when the first digital is not digital
+    ### You can add the regular expression you like
+    return data_frame.dropna()  # drop null
+    
+        
+"Show basic regression_statistic and print it to users" 
+def show_regression_statistic(excel_file):
+    df_regression = pd.DataFrame(excel_file)
+    df_regression = df_regression.drop(df_regression.columns[0],axis=1)
+    print("\n")
+    print_line()
+    print("The means for each column are:" )
+    print(df_regression.mean())
+    print_line()
+    print("The standard deviation for each column are:")
+    print(df_regression.std())
+    print_line()
+    print("The Maximun deviation for each column are:")
+    print(df_regression.max())
+    print_line()
+    print("The Mimnium deviation for each column are:")
+    print(df_regression.min())
+    print_line()
+    
+"Build regression model"
+def linear_regression(excel_file):
+    df = pd.DataFrame(excel_file)
+    #print(df)
+    #http://stackoverflow.com/questions/31593201/pandas-iloc-vs-ix-vs-loc-explanation/31593712#31593712
+    Y = df.iloc[:,1]  ##http://stackoverflow.com/questions/11285613/selecting-columns
+    #print(Y)
+    Xn = df.iloc[:,2:] 
+    # print(Xn)
+    lr = LinearRegression()  ##http://stackoverflow.com/questions/19991445/run-an-ols-regression-with-pandas-data-frame
+    lr.fit(Xn, Y)
+    return df, lr
+    
+def print_equation(df, lr):
+    print('coef is:  '+str(lr.coef_))
+    print('incpt is: '+str(lr.intercept_))
+    header_list = df.columns.values
+    #print(header_list)  ##http://stackoverflow.com/questions/19482970/get-list-from-pandas-dataframe-column-headers
+    print("\n")
+    print("The regression question is:")
+    reg_equation = str(header_list[1])+ '='
+    for i in range(0,len(lr.coef_)):
+        reg_equation += (str(lr.coef_[i]))
+        reg_equation += str(header_list[2+i])## 2 because the header_list include Date and y
+        reg_equation += ' + '
+    reg_equation += str(lr.intercept_)
+    print(reg_equation)
+    
+"Show number of null values and print it to users"    
+def show_number_of_null_values():
+    return
+def clean_all_null():
+    return
+    
+    
+    
+"Calculate the predicated value Y based on users' input and show the regression question"    
+def get_predictor(linear_equation):
+    print('Please enter the Xn in "X1,X2,X3...,Xn" format')
+    print("In your case, your n is "+str(len(linear_equation.coef_)))
+    usrinput = input(">>> Input: ")
+    print("Your input is "+usrinput) 
+    Xn_pred = re.split(",", usrinput)     #http://stackoverflow.com/questions/10974932/python-split-string-based-on-regular-expression
+    Xn_pred = [float(i) for i in Xn_pred] #float(i) for i in lst]
+    #old fashion way: for i in range(0,len(a)) :
+    #                     a[i] = float(a[i])
+    print(Xn_pred)
+    return Xn_pred
+
+
+def make_prediction(Xn_pred, lr, df):
+    result = 0
+    for i in range(0,len(lr.coef_)):
+        result += lr.coef_[i]*Xn_pred[i] 
+    result += lr.intercept_
+    print("\n")
+    print( str(df.columns.values[1])+' is predicted to be '+str(result) )
+    
+"Check which assumption is violated and print it to usrs"
+def check_hypothesis():
+    return
+    
+def print_line():
+    print("================================================================")
--- temp.py
+++ temp.py
@@ -6,137 +6,10 @@ This is a temporary script file.
 """
 
 
-import pandas as pd
-from sklearn.linear_model import LinearRegression
-import re   #regular expression
-from pandas.tools.plotting import scatter_matrix
-import matplotlib.pyplot as plt
-import numpy as np
-import time # to measure performance
 
+import functions # I think this is better than from functions import * when coming to maintance cost
 
-
-"User task: Ask user to inputs a file location and return xlxs"
-def user_input_file_location():
-    usrinput = input(">>> Input: ")
-    print("Your file location is "+usrinput)
-    return usrinput;
-    
-"System task: read xlxs and turn it into dataframe"
-def system_input_file(usrinput):
-    file_extension = detect_file_extension(usrinput)
-    
-    if(file_extension.lower()=="xlsx"):
-        file_input_to_system = pd.read_excel(usrinput)
-    if(file_extension.lower()=="csv"):
-        file_input_to_system = pd.read_csv(usrinput)
-    
-    
-    print("\n")
-    print(file_input_to_system.head())
-    return file_input_to_system
-    
-def detect_file_extension(usrinput):
-    file_extension = re.split("\.",usrinput) #get the extension name
-    return file_extension[1]
-
-
-def drop_space_nondigital(df):
-    data_frame = df.replace('\s+', np.nan, regex=True)   #http://stackoverflow.com/questions/26837998/pandas-replace-nan-with-blank-empty-string
-    data_frame = df.replace('[^\d]', np.nan, regex=True) #change the field to null when the first digital is not digital
-    ### You can add the regular expression you like
-    return data_frame.dropna()  # drop null
-    
-        
-"Show basic regression_statistic and print it to users" 
-def show_regression_statistic(excel_file):
-    df_regression = pd.DataFrame(excel_file)
-    df_regression = df_regression.drop(df_regression.columns[0],axis=1)
-    print("\n")
-    print_line()
-    print("The means for each column are:" )
-    print(df_regression.mean())
-    print_line()
-    print("The standard deviation for each column are:")
-    print(df_regression.std())
-    print_line()
-    print("The Maximun deviation for each column are:")
-    print(df_regression.max())
-    print_line()
-    print("The Mimnium deviation for each column are:")
-    print(df_regression.min())
-    print_line()
-    
-"Build regression model"
-def linear_regression(excel_file):
-    df = pd.DataFrame(excel_file)
-    #print(df)
-    #http://stackoverflow.com/questions/31593201/pandas-iloc-vs-ix-vs-loc-explanation/31593712#31593712
-    Y = df.iloc[:,1]  ##http://stackoverflow.com/questions/11285613/selecting-columns
-    #print(Y)
-    Xn = df.iloc[:,2:] 
-    # print(Xn)
-    lr = LinearRegression()  ##http://stackoverflow.com/questions/19991445/run-an-ols-regression-with-pandas-data-frame
-    lr.fit(Xn, Y)
-    return df, lr
-    
-def print_equation(df, lr):
-    print('coef is:  '+str(lr.coef_))
-    print('incpt is: '+str(lr.intercept_))
-    header_list = df.columns.values
-    #print(header_list)  ##http://stackoverflow.com/questions/19482970/get-list-from-pandas-dataframe-column-headers
-    print("\n")
-    print("The regression question is:")
-    reg_equation = str(header_list[1])+ '='
-    for i in range(0,len(lr.coef_)):
-        reg_equation += (str(lr.coef_[i]))
-        reg_equation += str(header_list[2+i])## 2 because the header_list include Date and y
-        reg_equation += ' + '
-    reg_equation += str(lr.intercept_)
-    print(reg_equation)
-    
-"Show number of null values and print it to users"    
-def show_number_of_null_values():
-    return
-def clean_all_null():
-    return
-    
-    
-    
-"Calculate the predicated value Y based on users' input and show the regression question"    
-def get_predictor(linear_equation):
-    print('Please enter the Xn in "X1,X2,X3...,Xn" format')
-    print("In your case, your n is "+str(len(linear_equation.coef_)))
-    usrinput = input(">>> Input: ")
-    print("Your input is "+usrinput) 
-    Xn_pred = re.split(",", usrinput)     #http://stackoverflow.com/questions/10974932/python-split-string-based-on-regular-expression
-    Xn_pred = [float(i) for i in Xn_pred] #float(i) for i in lst]
-    #old fashion way: for i in range(0,len(a)) :
-    #                     a[i] = float(a[i])
-    print(Xn_pred)
-    return Xn_pred
-
-
-def make_prediction(Xn_pred, lr, df):
-    result = 0
-    for i in range(0,len(lr.coef_)):
-        result += lr.coef_[i]*Xn_pred[i] 
-    result += lr.intercept_
-    print("\n")
-    print( str(df.columns.values[1])+' is predicted to be '+str(result) )
-    
-"Check which assumption is violated and print it to usrs"
-def check_hypothesis():
-    return
-    
-def print_line():
-    print("================================================================")
-
-
-
-"===============================================above is functions======================================================"
-
-"===============================================below is implementation================================================="
+"You should put functions.py and temp.py under same directory"
 
 #"1.ask usr to input the location of the file"
 print("Welcome, this is the beta version of statsitcal model application")
@@ -160,8 +33,8 @@ menu_parameter = True
 
 
         
-usrinput = user_input_file_location()
-df = system_input_file(usrinput)
+usrinput = functions.user_input_file_location()
+df = functions.system_input_file(usrinput)
 
 
 
@@ -181,54 +54,54 @@ while menu_parameter:
         break
     
     elif ans.lower()=="a":
-        usrinput = user_input_file_location()
-        df = system_input_file(usrinput) #This acts like a pointer and df is a global variable
+        usrinput = functions.user_input_file_location()
+        df = functions.system_input_file(usrinput) #This acts like a pointer and df is a global variable
         
     elif ans.lower()=="b":
-        start = time.time()
+        start = functions.time.time()
         print("\n")
         print(df)
-        end = time.time()
+        end = functions.time.time()
         print(" ")
         print("Time needed to implement this function: "+str(end - start))
         
     elif ans.lower()=="c":
-        start = time.time()
+        start = functions.time.time()
         row_number = input("How many row would you like to display   ")
         row_number = int(row_number)
         print("\n")
         print(str(df[:row_number]))
-        end = time.time()
+        end = functions.time.time()
         print(" ")
         print("Time needed to implement this function: "+str(end - start))
         
     elif ans.lower()=="d":
         
         print("non:")
-        start = time.time()
-        show_regression_statistic(df) # 3. show regression statistic
-        data_frame, linear_equation = linear_regression(df)
-        print_equation(data_frame,linear_equation)
-        end = time.time()
+        start = functions.time.time()
+        functions.show_regression_statistic(df) # 3. show regression statistic
+        data_frame, linear_equation = functions.linear_regression(df)
+        functions.print_equation(data_frame,linear_equation)
+        end = functions.time.time()
         print(" ")
         print("Time needed to implement this function: "+str(end - start))
         
         menu_parameter0 = input("Enter P to show matrix plot or anyother button to continue   ")
         if menu_parameter0.lower()=="p": # The graph doesn't show , i dont know why
-            start = time.time()
-            scatter_matrix(df,alpha=0.2, figsize=(6, 6), diagonal='kde')
-            plt.show()
+            start = functions.time.time()
+            functions.scatter_matrix(df,alpha=0.2, figsize=(6, 6), diagonal='kde')
+            functions.plt.show()
             
         menu_parameter1 = input("Enter <Yes> to make prediciton or <No> to exit the program or anyother button back to main menu   ")
         #Go into prediction section
         
         if menu_parameter1.lower()=="yes":
-            start = time.time()
+            start = functions.time.time()
             print("\n")
-            predictors = get_predictor(linear_equation)
-            make_prediction(predictors,linear_equation,data_frame)
+            predictors = functions.get_predictor(linear_equation)
+            functions.make_prediction(predictors,linear_equation,data_frame)
             print("\n")
-            end = time.time()
+            end = functions.time.time()
             print(" ")
             print("Time needed to implement this function: "+str(end - start))
             
@@ -237,12 +110,12 @@ while menu_parameter:
             
             
     elif ans.lower()=="e":
-            start = time.time()
-            data_frame_no_space_null = drop_space_nondigital(df)
+            start = functions.time.time()
+            data_frame_no_space_null = functions.drop_space_nondigital(df)
             menu_parameter2 = input("Enter <Yes> to save the change to your program or anyother button to cancel ")
             if menu_parameter2.lower() == "yes":
                 df = data_frame_no_space_null      #save the dataframe without any null to the previous dataframe, applying to the whole program
-            end = time.time()
+            end = functions.time.time()
             print(" ")
             print("Time needed to implement this function: "+str(end - start))
             
